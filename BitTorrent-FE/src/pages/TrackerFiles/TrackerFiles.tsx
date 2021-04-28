16b704d4242b9308015ab1e4f9638e8e979b1d97
= <sjablonski.it@gmail.com>
2021-04-28 11:55:22 +0200

    Modified TrackerFiles and UserFiles

--- BitTorrent-FE/src/pages/TrackerFiles/TrackerFiles.tsx
+++ BitTorrent-FE/src/pages/TrackerFiles/TrackerFiles.tsx
@@ -1,7 +1,8 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Button, Progress, Space, Table } from 'antd';
+import { DownloadOutlined } from '@ant-design/icons';
 import { getData, fetchTackerFiles } from 'redux/filesSlice';
-import FileList from 'components/FileList';
 
 function TrackerFiles() {
   const dataSource = useSelector(getData);
@@ -11,7 +12,66 @@ function TrackerFiles() {
     dispatch(fetchTackerFiles());
   }, [dispatch]);
 
-  return <FileList dataSource={dataSource} />;
+  const columns = [
+    {
+      title: 'id',
+      key: 'id',
+      dataIndex: 'id',
+    },
+    {
+      title: 'Nazwa pliku',
+      key: 'name',
+      dataIndex: 'name',
+    },
+    {
+      title: 'Rozmiar pliku',
+      key: 'size',
+      dataIndex: 'size',
+    },
+    {
+      title: 'Liczba kopii pliku',
+      key: 'size',
+      render: (text: string, record: any) => {
+        return record.users.length;
+      },
+    },
+    {
+      title: 'Akcje',
+      key: 'action',
+      render: () => {
+        return (
+          <Space size={[12, 12]} wrap>
+            <Button icon={<DownloadOutlined />}>Pobierz</Button>
+          </Space>
+        );
+      },
+    },
+  ];
+
+  return (
+    <Table
+      dataSource={dataSource}
+      rowKey="id"
+      columns={columns}
+      tableLayout="fixed"
+      pagination={false}
+      expandable={{
+        expandedRowRender: (record) => (
+          <>
+            {record.users?.map((user: any) => {
+              return (
+                <div key={user.id} style={{ marginBottom: '12px' }}>
+                  ID: {user.id}
+                  <Progress percent={30} />
+                </div>
+              );
+            })}
+          </>
+        ),
+        rowExpandable: (record) => record.users !== undefined && record.users.length > 0,
+      }}
+    />
+  );
 }
 
 export default TrackerFiles;
--- BitTorrent-FE/src/pages/UserFiles/UserFiles.tsx
+++ BitTorrent-FE/src/pages/UserFiles/UserFiles.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Table } from 'antd';
 import { getData, fetchTackerFiles } from 'redux/filesSlice';
-import FileList from 'components/FileList';
 
 function UserFiles() {
   const dataSource = useSelector(getData);
@@ -11,7 +11,33 @@ function UserFiles() {
     dispatch(fetchTackerFiles());
   }, [dispatch]);
 
-  return <FileList dataSource={dataSource} />;
+  const columns = [
+    {
+      title: 'id',
+      key: 'id',
+      dataIndex: 'id',
+    },
+    {
+      title: 'Nazwa pliku',
+      key: 'name',
+      dataIndex: 'name',
+    },
+    {
+      title: 'Rozmiar pliku',
+      key: 'size',
+      dataIndex: 'size',
+    },
+  ];
+
+  return (
+    <Table
+      dataSource={dataSource}
+      rowKey="id"
+      columns={columns}
+      tableLayout="fixed"
+      pagination={false}
+    />
+  );
 }
 
 export default UserFiles;
