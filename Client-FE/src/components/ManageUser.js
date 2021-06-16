28db3c6fdb3558fd9638addaf16168b9165298db
= <sjablonski.it@gmail.com>
2021-06-16 08:35:23 +0200

    Fix setState

--- Client-FE/src/components/ManageUser.js
+++ Client-FE/src/components/ManageUser.js
@@ -1,53 +1,80 @@
-import React,{Component} from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
 export class ManageUser extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      login: "",
+      button: 1,
+    };
+  }
 
-    constructor (props) {
-        super(props);
-        this.state = {
-            login: "",
-            button: 1
-        }
-    }
+  changeHandler = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-    changeHandler = (e) => {
-        this.setState({[e.target.name]: e.target.value});
-    }
-
-    submitHandler = (e) => {
-        e.preventDefault();
-        console.log(this.state.login);
-        if(this.state.button === 1){
-            axios.post('http://localhost:8081/client/users/'+this.state.login+'/enable')
-            .then(response => {
-                console.log(response)
-            })
-            .catch(error => {
-                console.log(error)
-            })
-        }
-        if(this.state.button === 2){
-            axios.post('http://localhost:8081/client/users/'+this.state.login+'/disable')
-            .then(response => {
-                console.log(response)
-            })
-            .catch(error => {
-                console.log(error)
-            })
-        }
-    }
-
-    render(){
-        return (
-            <div className="manageUser">
-                <h2>Włącz / wyłącz użytkownika</h2>
-                <form onSubmit ={this.submitHandler}>
-                    <input type="text" placeholder="login" name = "login" value={this.state.login} onChange={this.changeHandler}></input>
-                    <button onClick={() => (this.state.button = 1)} type="submit" className="enableBtn">Włącz</button>
-                    <button onClick={() => (this.state.button = 2)} type="submit" className="disableBtn">Wyłącz</button>
-                </form>
-            </div>
+  submitHandler = (e) => {
+    e.preventDefault();
+    console.log(this.state.login);
+    if (this.state.button === 1) {
+      axios
+        .post(
+          "http://localhost:8081/client/users/" + this.state.login + "/enable"
         )
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     }
+    if (this.state.button === 2) {
+      axios
+        .post(
+          "http://localhost:8081/client/users/" + this.state.login + "/disable"
+        )
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
+
+  handleClick = (flag) => {
+    this.setState({ button: flag });
+  };
+
+  render() {
+    return (
+      <div className="manageUser">
+        <h2>Włącz / wyłącz użytkownika</h2>
+        <form onSubmit={this.submitHandler}>
+          <input
+            type="text"
+            placeholder="login"
+            name="login"
+            value={this.state.login}
+            onChange={this.changeHandler}
+          ></input>
+          <button
+            onClick={() => this.handleClick(1)}
+            type="submit"
+            className="enableBtn"
+          >
+            Włącz
+          </button>
+          <button
+            onClick={() => this.handleClick(2)}
+            type="submit"
+            className="disableBtn"
+          >
+            Wyłącz
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
